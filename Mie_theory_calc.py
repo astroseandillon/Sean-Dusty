9d4a8dbf966b4e768a0b0c8ff77793452003e74f
Sean <seandillon48@gmail.com>
2025-10-17 10:45:50 -0500

    edited mie theory calcs to be able to output calculations

--- Mie_theory_calc.py
+++ Mie_theory_calc.py
@@ -7,7 +7,30 @@ Created on Fri Oct 17 09:07:22 2025
 """
 
 import numpy as np
+import scipy.integrate as spit
 import matplotlib.pyplot as plt
+dust_dir = ['/home/physics/Research/DUSTY/DUSTY/Lib_nk/', 
+            "C:/UTSA/Research/DUSTY/DUSTY/Lib_nk/",
+           "C:/Users/uhe082/OneDrive - University of Texas at San Antonio (1)/Lib_nk/"]
+# this is the possible locations of where dust can be
+
+
+nk_path = dust_dir[2]               #where the dust is 
+
+def volume_integrand_mrn(r, q):
+    v = r**(-q)
+    return v
+
+# UNITS ARE IN MICRONS
+rmin = 0.005
+rmax = 0.25
+q = 3.5
+
+r_integral = spit.quad(volume_integrand_mrn, rmin, rmax, args=q)
+r_average = ((1/(rmax - rmin)) * r_integral[0])**(1/-q)
+
+
+#creating the mie theory calc function
 
 def q_sca(r, m, lam):
     q = (8/3) * ((2*np.pi*r/lam)**4) * np.real(((m**2 - 1)/(m**2 + 2))**2)
@@ -17,4 +40,15 @@ def q_abs(r, m, lam):
     q = (8*np.pi*r/lam) * np.imag((m**2 -1)/(m**2 + 2))
     return q
 
+
+dname = ['beta-SiC.nk']
+lam, dust_n, dust_k = np.loadtxt(nk_path+dname[0], unpack=True)
+
+
+
 print('hello world')
+
+
+
+
+
